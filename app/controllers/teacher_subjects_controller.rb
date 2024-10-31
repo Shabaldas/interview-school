50dd4d51753877de49de1f9c7c120a955f0b6a74
Volodymyr Shabaldas <shabaldasengr@gmail.com>
2024-10-31 23:51:50 +0200

    BE/FE (feat): add old features code structure

---
+++ app/controllers/teacher_subjects_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class TeacherSubjectsController < ApplicationController
+  before_action :_set_teacher_subject, only: [:show, :edit, :update, :destroy]
+  before_action :_set_teacher, only: [:new, :create]
+
+  def show; end
+
+  def new
+    @teacher_subject = TeacherSubject.new level: TeacherSubject::DEFAULT_LEVEL
+  end
+
+  def edit; end
+
+  def create
+    @teacher_subject = TeacherSubject.new _teacher_subject_params
+    @teacher_subject.teacher = @teacher
+    if @teacher_subject.save
+      flash[:notice] = 'Class was successfully added'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @teacher_subject.update(_teacher_subject_params)
+      render :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @teacher_subject.destroy!
+    flash.now[:notice] = 'Class was successfully removed'
+  end
+
+  private
+
+  def _set_teacher_subject
+    @teacher_subject = TeacherSubject.find params[:id]
+  end
+
+  def _set_teacher
+    @teacher = Teacher.find params[:teacher_id]
+  end
+
+  def _teacher_subject_params
+    params.require(:teacher_subject).permit(:level, :subject_id)
+  end
+end
---
+++ app/models/subject.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Subject < ApplicationRecord
+  has_many :teacher_subjects, dependent: :destroy
+  has_many :teachers, through: :teacher_subjects
+  accepts_nested_attributes_for :teacher_subjects, allow_destroy: true
+
+  validates :name, presence: true
+end
