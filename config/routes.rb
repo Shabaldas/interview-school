a5d4206ca5cee6a93a795d9d9c23902bf50a5569
Volodymyr Shabaldas <shabaldasengr@gmail.com>
2024-11-05 15:19:13 +0200

    BE/FE (feat): update home contoller, add section contoller

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,14 @@ Rails.application.routes.draw do
   get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
   get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :sections, only: [:index, :show] do
+    get 'available', on: :collection
+
+    member do
+      post 'enroll'
+      delete 'withdraw'
+    end
+  end
 
   resources :teachers do
     resources :teacher_subjects, shallow: true
